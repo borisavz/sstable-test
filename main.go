d003c623879f182d56fc7b219ce4bb16dad96067
borisavz <borisavzivanovic@gmail.com>
2023-04-01 13:11:59 +0200

    proper serialization and deserialization of index

--- main.go
+++ main.go
@@ -2,24 +2,38 @@ package main
 
 import (
 	"encoding/binary"
+	"fmt"
 	"github.com/huandu/skiplist"
 	"os"
 )
 
 type DataEntry struct {
-	keySize   int32
-	valueSize int32
+	keySize   uint32
+	valueSize uint32
 	key       string
 	value     []byte
 }
 
+func (d DataEntry) String() string {
+	return fmt.Sprintf("DataEntry[keySize: %d, valueSize: %d, key: %s, value: ???]", d.keySize, d.valueSize, d.key)
+}
+
 type IndexEntry struct {
-	keySize    int32
+	keySize    uint32
 	key        string
-	dataOffset int32
+	dataOffset uint32
+}
+
+func (i IndexEntry) String() string {
+	return fmt.Sprintf("IndexEntry[keySize: %d, key: %s, dataOffset: %d]", i.keySize, i.key, i.dataOffset)
 }
 
 func main() {
+	Store()
+	Load()
+}
+
+func Store() {
 	list := skiplist.New(skiplist.StringAsc)
 
 	list.Set("a", []byte{1})
@@ -47,36 +61,86 @@ func main() {
 		strKey := el.Key().(string)
 		binVal := el.Value.([]byte)
 
+		binKey := []byte(strKey)
+		binKeySize := binary.Size(binKey)
+
 		data := DataEntry{
-			keySize:   int32(binary.Size([]byte(strKey))),
-			valueSize: int32(binary.Size(binVal)),
+			keySize:   uint32(binKeySize),
+			valueSize: uint32(binary.Size(binVal)),
 			key:       strKey,
 			value:     binVal,
 		}
 
-		dataSize := binary.Size(data.keySize) + binary.Size(data.valueSize) + binary.Size(data.key) + binary.Size(data.value)
+		dataSize := binary.Size(data.keySize) + binary.Size(data.valueSize) + int(binKeySize) + int(data.valueSize)
 
 		index := IndexEntry{
-			keySize:    int32(binary.Size([]byte(strKey))),
+			keySize:    uint32(binKeySize),
 			key:        strKey,
-			dataOffset: int32(dataOffset),
+			dataOffset: uint32(dataOffset),
 		}
 
-		indexSize := binary.Size(index.keySize) + binary.Size(index.key) + binary.Size(index.dataOffset)
+		indexSize := binary.Size(index.keySize) + int(binKeySize) + binary.Size(index.dataOffset)
 
 		binary.Write(dataFile, binary.BigEndian, data.keySize)
 		binary.Write(dataFile, binary.BigEndian, data.valueSize)
-		binary.Write(dataFile, binary.BigEndian, data.key)
+		binary.Write(dataFile, binary.BigEndian, binKey)
 		binary.Write(dataFile, binary.BigEndian, data.value)
 
 		binary.Write(indexFile, binary.BigEndian, index.keySize)
-		binary.Write(indexFile, binary.BigEndian, index.key)
+		binary.Write(indexFile, binary.BigEndian, binKey)
 		binary.Write(indexFile, binary.BigEndian, index.dataOffset)
+
+		println(index.String())
+		println(data.String())
+		println("---")
 
 		dataOffset += dataSize
 		indexOffset += indexSize
 
 		el = el.Next()
+	}
+
+	indexFile.Close()
+	dataFile.Close()
+}
+
+func Load() {
+	indexFile, err := os.Open("index.bin")
+	if err != nil {
+		panic(err)
+	}
+
+	dataFile, err := os.Open("data.bin")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		keySizeBin := make([]byte, 4)
+		err := binary.Read(indexFile, binary.BigEndian, keySizeBin)
+		if err != nil {
+			break
+		}
+
+		keySize := binary.BigEndian.Uint32(keySizeBin)
+
+		keyBin := make([]byte, keySize)
+		binary.Read(indexFile, binary.BigEndian, keyBin)
+
+		key := string(keyBin)
+
+		dataOffsetBin := make([]byte, 4)
+		binary.Read(indexFile, binary.BigEndian, dataOffsetBin)
+
+		dataOffset := binary.BigEndian.Uint32(dataOffsetBin)
+
+		index := IndexEntry{
+			keySize:    keySize,
+			key:        key,
+			dataOffset: dataOffset,
+		}
+
+		println(index.String())
 	}
 
 	indexFile.Close()
